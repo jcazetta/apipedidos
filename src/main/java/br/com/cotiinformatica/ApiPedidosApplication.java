0d8df1d56b9b9a8f3eefc32b2eddcf4a2bb1a900
jcazetta <cazettajunior@gmail.com>
2025-06-30 17:19:28 -0300

    test adiciona testes unitarios e testes de integracao

--- src/main/java/br/com/cotiinformatica/ApiPedidosApplication.java
+++ src/main/java/br/com/cotiinformatica/ApiPedidosApplication.java
@@ -1,13 +1,15 @@
 package br.com.cotiinformatica;
-
+import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+@EnableRabbit
 @SpringBootApplication
 public class ApiPedidosApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(ApiPedidosApplication.class, args);
 	}
-
 }
+
+
